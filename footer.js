74ede65f6478cbfd09a8d3f66e300dd10f895739
Iryna <79843845+shumakovaira@users.noreply.github.com>
2022-07-08 01:19:33 +0300

    init

---
+++ footer.js
@@ -0,0 +1,8 @@
+let appFooter = `
+<footer class="tm-footer">
+    <p class="text-xs-center">All rights reserved &copy; 2021</p>
+    <br> 
+</footer>
+`;
+
+document.getElementById("app-footer").innerHTML = appFooter;
---
+++ header.js
@@ -0,0 +1,18 @@
+let appHeader = `
+<header>
+    <img src="img/logo.png" width=960px alt="travel-logo">
+</header>
+
+<div id="navBar">
+    <nav>
+      <a href="index.html">Home</a>
+      <a href="about.html">About</a>
+      <a href="story.html">Blog</a>
+      <a href="gallery.html">Portfolio</a>
+      <a href="contact.html">Contact</a>
+      <div class="animation start-home"></div>
+    </nav>
+</div> 
+`;
+
+document.getElementById("app-header").innerHTML = appHeader;
